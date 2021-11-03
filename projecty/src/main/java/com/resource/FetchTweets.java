00f2d41f0637f549ba103d6a2756f02878626257
Kartik Gour <89845360+Kgaur624@users.noreply.github.com>
2021-11-03 12:00:27 +0530

    pom file updated

--- projecty/src/main/java/com/resource/FetchTweets.java
+++ projecty/src/main/java/com/resource/FetchTweets.java
@@ -1,32 +1,31 @@
 package com.resource;
 
+import com.config.DropWizardConfiguration;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
+import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
 
 class FetchTweets {
-    public static List<String> latestTweet() throws TwitterException {
-        List<String> lst = new ArrayList<String>();
-        ConfigurationBuilder cb = new ConfigurationBuilder();
-        cb.setDebugEnabled(true)
-                .setOAuthConsumerKey("jUw2qaXkcqirbpR0h76dGQPEb")
-                .setOAuthConsumerSecret("KlrnGLxD3QfQatAidusemOX8T7OL072Vi2HJppCMrxGL2svkcY")
-                .setOAuthAccessToken("1450680649031962626-FlE4DbG86871LzJ6dVrA77aMjR47cy")
-                .setOAuthAccessTokenSecret("gEIxm1eWf6ON47WszmZMzm6v9rtzWTo4LGJtIdygsJFeU");
-        TwitterFactory tf = new TwitterFactory(cb.build());
+    public static Response latestTweet() throws TwitterException {
+        List<String> list = new ArrayList<String>();
+
+        ConfigurationBuilder configurationBuilder = DropWizardConfiguration.getConfigurationObject();
+
+        TwitterFactory tf = new TwitterFactory(configurationBuilder.build());
         Twitter twitter = tf.getInstance();
         List<Status> statuses = twitter.getHomeTimeline();
-        for (Status status1 : statuses) {
-            lst.add(status1.getText());
+        for (Status status : statuses) {
+            list.add(status.getText());
         }
-        if (lst.isEmpty()) {
-            lst.add("No Tweet Found ");
+        if (list.isEmpty()) {
+            list.add("No Tweet Found ");
         }
-        return lst;
+        return Response.ok(list).build();
     }
 }
